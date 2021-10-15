fb0afb2ee0d259cbdc1aa203618f1749df537056
= <nexrey19@gmail.com>
2021-10-15 22:10:57 +0700

    Menyiapkan Data Wisata

--- app/src/main/java/org/genadidharma/jjjyuk/Wisata.java
+++ app/src/main/java/org/genadidharma/jjjyuk/Wisata.java
@@ -1,4 +1,60 @@
 package org.genadidharma.jjjyuk;
 
 public class Wisata {
+//    Deklrasi Variable
+    private int foto;
+    private String nama_wisata , deskipsi , jam , alamat ,harga_tiket;
+
+
+//    Create Setter and Getter
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNama_wisata() {
+        return nama_wisata;
+    }
+
+    public void setNama_wisata(String nama_wisata) {
+        this.nama_wisata = nama_wisata;
+    }
+
+    public String getDeskipsi() {
+        return deskipsi;
+    }
+
+    public void setDeskipsi(String deskipsi) {
+        this.deskipsi = deskipsi;
+    }
+
+    public String getJam() {
+        return jam;
+    }
+
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getHarga_tiket() {
+        return harga_tiket;
+    }
+
+    public void setHarga_tiket(String harga_tiket) {
+        this.harga_tiket = harga_tiket;
+    }
+
+
 }
---
+++ app/src/main/java/org/genadidharma/jjjyuk/WisataData.java
@@ -0,0 +1,50 @@
+package org.genadidharma.jjjyuk;
+
+import java.util.ArrayList;
+import java.util.List;
+public class WisataData {
+
+    private static final String[] foto = {
+            "https://promoliburan.com/userfiles/uploads/batu-night-spectacular-bns-10.jpg",
+            "https://images6.alphacoders.com/378/thumb-1920-378352.jpg",
+            "https://i.pinimg.com/originals/0f/d0/89/0fd0890e0133d1a9413ac949c1928baa.jpg",
+            "https://1.bp.blogspot.com/-NlJvqolPrx4/Wa1HiaclzDI/AAAAAAAAG_s/SghfTHgMsAsD5PsEZYPAmb_OnqSgY19BACLcBGAs/w1280-h720-p-k-no-nu/pantai%2B3%2Bwarna.jpg",
+            "https://www.malang-guidance.com/wp-content/uploads/2012/06/Taman-Wisata-Selecta-Batu-Malang-MG.jpg"
+    };
+
+    private static final String[] nama_wisata={
+            "Batu Night Spectacular",
+            "Gunung Bromo",
+            "Air Terjun Coban Pelangi",
+            "Pantai 3 Warna",
+            "Taman Rekreasi Selecta",
+    };
+
+    private static final String[] deskripsi={
+
+    };
+
+    private static final String[] jam ={
+            "15.00 - 21.00",
+            "24 Jam",
+            "08.00 - 17.00",
+            "07.00 - 14.00",
+            "07.00 - 16.00",
+    };
+
+    private static final String[] alamat={
+            "Jl. Hayam Wuruk, Oro-Oro Ombo, Kec.Batu, Kota Batu",
+            "Taman Nasional Bromo Tengger Semeru",
+            "Dusun Ngadas, Ngadas, Poncokusumo, Malang",
+            "Jl. Sendang Biru, Tambakrejo, Sumbermanjing, Malang",
+            "Jl. Raya Selecta No.1, Tulungrejo, Kec. Bumiaji, Kota Batu"
+    };
+
+    private static final String[] harga_tiket={
+            "35 Ribu",
+            "32 Ribu",
+            "10 Ribu",
+            "10 Ribu",
+            "40 Ribu"
+    };
+}
