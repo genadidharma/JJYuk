5fbd445b2afbf2144d502a8fa0fa05074cdc2dd2
Genadi Dharma <genadislawa@gmail.com>
2021-12-01 12:17:48 +0800

    destination update data when unlike fixed

--- app/src/main/java/org/genadidharma/jjjyuk/ui/destination/domain/DestinationFavoriteActivity.java
+++ app/src/main/java/org/genadidharma/jjjyuk/ui/destination/domain/DestinationFavoriteActivity.java
@@ -3,23 +3,12 @@ package org.genadidharma.jjjyuk.ui.destination.domain;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import org.genadidharma.jjjyuk.MainActivity;
 import org.genadidharma.jjjyuk.R;
-import org.genadidharma.jjjyuk.data.model.Destination;
 import org.genadidharma.jjjyuk.db.AppDatabase;
 import org.genadidharma.jjjyuk.db.Dest;
 import org.genadidharma.jjjyuk.ui.destination.DestinationAdapter;
@@ -29,7 +18,7 @@ import org.genadidharma.jjjyuk.util.GridSpacingItemDecoration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Destination_Favorite extends AppCompatActivity {
+public class DestinationFavoriteActivity extends AppCompatActivity {
 
     public static final String EXTRA_KEY_DESTINATION_TYPE = "type";
     public static final String EXTRA_KEY_DESTINATION_NAME = "dest";
@@ -71,10 +60,18 @@ public class Destination_Favorite extends AppCompatActivity {
             public void onRefresh() {
                 initLayout();
                 iniData();
-                setupAdapterOnRefresh(dest);
+                adapter.updateData(dest);
                 srlRefresh.setRefreshing(false);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initLayout();
+        iniData();
+        adapter.updateData(dest);
     }
 
     private void initLayout() {
@@ -87,8 +84,8 @@ public class Destination_Favorite extends AppCompatActivity {
     }
 
     private void setupAdapter(List<Dest> list) {
-        adapter = new DestinationAdapterFav(list,Destination_Favorite.this, (dest) -> {
-            Intent intent = new Intent(Destination_Favorite.this, DestinationDetailActivity.class);
+        adapter = new DestinationAdapterFav(list, DestinationFavoriteActivity.this, (dest) -> {
+            Intent intent = new Intent(DestinationFavoriteActivity.this, DestinationDetailActivity.class);
             intent.putExtra(EXTRA_KEY_DESTINATION_TYPE, (dest.getJenis().equals(DestinationAdapter.KEY_IMAGE)) ? DestinationAdapter.LAYOUT_IMAGE : DestinationAdapter.LAYOUT_VIDEO);
             intent.putExtra(EXTRA_KEY_DESTINATION_NAME, dest.getNama_wisata());
             intent.putExtra(EXTRA_KEY_DESTINATION_ADDRESS, dest.getAlamat());
@@ -118,38 +115,6 @@ public class Destination_Favorite extends AppCompatActivity {
                 true,
                 0));
 
-        rv_fav.setAdapter(adapter);
-    }
-
-    private void setupAdapterOnRefresh(List<Dest> list) {
-        adapter = new DestinationAdapterFav(list,Destination_Favorite.this, (dest) -> {
-            Intent intent = new Intent(Destination_Favorite.this, DestinationDetailActivity.class);
-            intent.putExtra(EXTRA_KEY_DESTINATION_TYPE, (dest.getJenis().equals(DestinationAdapter.KEY_IMAGE)) ? DestinationAdapter.LAYOUT_IMAGE : DestinationAdapter.LAYOUT_VIDEO);
-            intent.putExtra(EXTRA_KEY_DESTINATION_NAME, dest.getNama_wisata());
-            intent.putExtra(EXTRA_KEY_DESTINATION_ADDRESS, dest.getAlamat());
-            intent.putExtra(EXTRA_KEY_DESTINATION_DESCRIPTION, dest.getDeskripsi());
-            intent.putExtra(EXTRA_KEY_DESTINATION_PRICE, dest.getHarga_tiket());
-            intent.putExtra(EXTRA_KEY_DESTINATION_PROTOCOL, dest.getProtokol());
-            intent.putExtra(EXTRA_KEY_DESTINATION_TIME, dest.getJam_buka() + "-" + dest.getJam_tutup());
-            intent.putExtra(EXTRA_KEY_DESTINATION_RATING, dest.getRating());
-            intent.putExtra(EXTRA_KEY_DESTINATION_STATUS, dest.getStatus());
-            intent.putExtra(EXTRA_KEY_DESTINATION_REVIEW, dest.getUlasan());
-            intent.putExtra(EXTRA_KEY_DESTINATION_IMAGE, dest.getFoto());
-            intent.putExtra(EXTRA_KEY_DESTINATION_VIDEO, dest.getVideo());
-            intent.putExtra(EXTRA_KEY_DESTINATION_DISTANCE, dest.getJarak());
-            intent.putExtra(EXTRA_KEY_DESTINATION_CLOSE, dest.getJam_tutup());
-            intent.putExtra(EXTRA_KEY_DESTINATION_OPEN, dest.getJam_buka());
-            intent.putExtra(EXTRA_KEY_DESTINATION_LATITUDE, dest.getLatitude());
-            intent.putExtra(EXTRA_KEY_DESTINATION_LONGITUDE, dest.getLongitude());
-            startActivity(intent);
-        });
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        rv_fav.setLayoutManager(layoutManager);
-        rv_fav.addItemDecoration(new GridSpacingItemDecoration(
-                2,
-                getResources().getDimensionPixelSize(R.dimen.sm_margin_padding),
-                true,
-                5));
         rv_fav.setAdapter(adapter);
     }
 
