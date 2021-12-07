e1647e098560abc5d4557dce2a369e6a3dbe3e4f
Genadi Dharma <genadislawa@gmail.com>
2021-12-07 13:41:23 +0800

    map marker added with MapBox API

--- app/src/main/java/org/genadidharma/jjjyuk/ui/destination/domain/DestinationDetailActivity.java
+++ app/src/main/java/org/genadidharma/jjjyuk/ui/destination/domain/DestinationDetailActivity.java
@@ -1,22 +1,29 @@
 package org.genadidharma.jjjyuk.ui.destination.domain;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.bumptech.glide.Glide;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 import org.genadidharma.jjjyuk.MainActivity;
 import org.genadidharma.jjjyuk.R;
@@ -27,14 +34,16 @@ import org.genadidharma.jjjyuk.ui.destination.DestinationAdapterFav;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class DestinationDetailActivity extends AppCompatActivity {
+public class DestinationDetailActivity extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener {
 
-    private String title, image, video, distance, description, time, address, price, status, protocol,jam_buka,jam_tutup , id;
-    private double rating , lat , longitude;
+    private String title, image, video, distance, description, time, address, price, status, protocol, jam_buka, jam_tutup, id;
+    private double rating, lat, longitude;
     private int type, review;
 
     private TextView tvTitle, tvPrice, tvDistance, tvRating, tvReview, tvAddress, tvTime, tvStatus, tvProtocol, tvDescription;
+    private MapView mvMap;
     private ShapeableImageView ivImage;
     private ImageView ivFgVideo, ivPlayVideo;
     private Toolbar toolbar;
@@ -42,31 +51,38 @@ public class DestinationDetailActivity extends AppCompatActivity {
     AppDatabaseDest database;
     private String jenis;
     boolean found = true;
-    DestinationAdapterFav adapter;
-    DestinationAdapter adapter_dest;
+
+    MapboxMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_destination_detail);
+
         database = AppDatabaseDest.getInstance(this);
 
         initLayout();
         getIntentExtra();
         initButtonFavorite();
 
+        mvMap.onCreate(savedInstanceState);
+        mvMap.getMapAsync(this);
+
         ImageButton favoriteButton = findViewById(R.id.btn_fav);
-        favoriteButton.setOnClickListener(new View.OnClickListener(){
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 List<Destination> dest = new ArrayList<>();
                 dest = database.destDao().getAll();
-                if (database.destDao().getRowCount() == 0){
+                if (database.destDao().getRowCount() == 0) {
                     insertNewDestination();
                     favoriteButton.setImageResource(R.drawable.ic_baseline_favorite_24);
                     autoRefresh();
-                }else{
-                    for (int i = 0; i < database.destDao().getRowCount() ; i++) {
+                } else {
+                    for (int i = 0; i < database.destDao().getRowCount(); i++) {
                         if (dest.get(i).getNamaWisata().equalsIgnoreCase(title)) {
                             database.destDao().deleteOne(dest.get(i));
 
@@ -77,8 +93,8 @@ public class DestinationDetailActivity extends AppCompatActivity {
                             break;
                         }
                     }
-                    if (found == true){
-                      insertNewDestination();
+                    if (found == true) {
+                        insertNewDestination();
                         favoriteButton.setImageResource(R.drawable.ic_baseline_favorite_24);
                         autoRefresh();
                     }
@@ -132,18 +148,18 @@ public class DestinationDetailActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    private void initButtonFavorite(){
+    private void initButtonFavorite() {
         List<Destination> dest = new ArrayList<>();
         ImageButton favoriteButton = findViewById(R.id.btn_fav);
         boolean foundFav = false;
         dest = database.destDao().getAll();
         for (int i = 0; i < database.destDao().getRowCount(); i++) {
-            if (dest.get(i).getNamaWisata().equalsIgnoreCase(title)){
+            if (dest.get(i).getNamaWisata().equalsIgnoreCase(title)) {
                 favoriteButton.setImageResource(R.drawable.ic_baseline_favorite_24);
-                foundFav=true;
+                foundFav = true;
             }
         }
-        if (foundFav == false){
+        if (foundFav == false) {
             favoriteButton.setImageResource(R.drawable.ic_favorite_border_24dp);
         }
     }
@@ -155,6 +171,7 @@ public class DestinationDetailActivity extends AppCompatActivity {
         tvDescription = findViewById(R.id.tv_description);
         tvPrice = findViewById(R.id.tv_price);
         tvProtocol = findViewById(R.id.tv_health_safety_protocol);
+        mvMap = findViewById(R.id.mv_map);
         tvTime = findViewById(R.id.tv_time);
         tvRating = findViewById(R.id.tv_rating);
         tvStatus = findViewById(R.id.tv_status);
@@ -182,12 +199,12 @@ public class DestinationDetailActivity extends AppCompatActivity {
         jam_buka = getIntent().getStringExtra(MainActivity.EXTRA_KEY_DESTINATION_OPEN);
         jam_tutup = getIntent().getStringExtra(MainActivity.EXTRA_KEY_DESTINATION_CLOSE);
         id = getIntent().getStringExtra(MainActivity.EXTRA_KEY_DESTINATION_ID);
-        lat = getIntent().getDoubleExtra(MainActivity.EXTRA_KEY_DESTINATION_LATITUDE,0);
-        longitude = getIntent().getDoubleExtra(MainActivity.EXTRA_KEY_DESTINATION_LONGITUDE,0);
+        lat = getIntent().getDoubleExtra(MainActivity.EXTRA_KEY_DESTINATION_LATITUDE, 0);
+        longitude = getIntent().getDoubleExtra(MainActivity.EXTRA_KEY_DESTINATION_LONGITUDE, 0);
 
     }
 
-    private void insertNewDestination(){
+    private void insertNewDestination() {
         Destination data = new Destination();
         if (type == 0) {
             jenis = "gambar";
@@ -219,10 +236,77 @@ public class DestinationDetailActivity extends AppCompatActivity {
         dataList.clear();
     }
 
-    private void autoRefresh(){
+    private void autoRefresh() {
         finish();
         overridePendingTransition(0, 0);
         startActivity(getIntent());
         overridePendingTransition(0, 0);
+    }
+
+    private void addMapMarker() {
+        LatLng location = new LatLng(lat, longitude);
+        map.addMarker(new MarkerOptions().position(location));
+        map.setCameraPosition(
+                new CameraPosition.Builder()
+                        .target(location)
+                        .build()
+        );
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        String uri = String.format(Locale.ENGLISH, "geo:%f,%f", lat, longitude);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+        map.addOnMapClickListener(this);
+
+        addMapMarker();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mvMap.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mvMap.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mvMap.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mvMap.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mvMap.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mvMap.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mvMap.onLowMemory();
     }
 }
--- app/src/main/java/org/genadidharma/jjjyuk/ui/splash/SplashScreenActivity.java
+++ app/src/main/java/org/genadidharma/jjjyuk/ui/splash/SplashScreenActivity.java
@@ -1,21 +1,29 @@
 package org.genadidharma.jjjyuk.ui.splash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import org.genadidharma.jjjyuk.MainActivity;
 import org.genadidharma.jjjyuk.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1002;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
 
+    private void intent(){
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
             startActivity(intent);
@@ -23,4 +31,28 @@ public class SplashScreenActivity extends AppCompatActivity {
             finish();
         }, 1500);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_ASK_PERMISSIONS) {
+            intent();
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            int res = checkSelfPermission(android.Manifest.permission.READ_PHONE_STATE);
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE_ASK_PERMISSIONS);
+            }else{
+                intent();
+            }
+        }
+    }
+
 }
